2cb1dd64ac101bf0a51bfc11324137d1c45c712a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-10-20 13:16:28 +0300

    added fields and methods to the class

--- src/main/java/io/sancta/sanctorum/dao/CityDAO.java
+++ src/main/java/io/sancta/sanctorum/dao/CityDAO.java
@@ -1,4 +1,31 @@
 package io.sancta.sanctorum.dao;
 
+import io.sancta.sanctorum.domain.City;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CityDAO {
+
+    SessionFactory sessionFactory;
+
+    public List<City> getItems(int offset, int limit) {
+        String hql = "select c from City as c";
+        Query<City> query = sessionFactory.getCurrentSession().createQuery(hql, City.class);
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+        return query.list();
+    }
+
+    public int getTotalCount() {
+        String hql = "select count(c) from City as c";
+        Query<Long> query = sessionFactory.getCurrentSession().createQuery(hql, Long.class);
+        return Math.toIntExact(query.uniqueResult());
+    }
 }
--- src/main/java/io/sancta/sanctorum/dao/CountryDAO.java
+++ src/main/java/io/sancta/sanctorum/dao/CountryDAO.java
@@ -1,4 +1,23 @@
 package io.sancta.sanctorum.dao;
 
+import io.sancta.sanctorum.domain.Country;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CountryDAO {
+
+    SessionFactory sessionFactory;
+
+    public List<Country> getAll() {
+        String hql = "select c from Country as c";
+        Query<Country> query = sessionFactory.getCurrentSession().createQuery(hql, Country.class);
+        return query.list();
+    }
 }

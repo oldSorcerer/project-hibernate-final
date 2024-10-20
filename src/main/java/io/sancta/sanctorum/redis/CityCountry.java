aad8bcaeeefcd8c1f7a1d2d26777599d2cf6063d
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-10-20 20:52:58 +0300

    added fields to the clas from redis

--- src/main/java/io/sancta/sanctorum/redis/CityCountry.java
+++ src/main/java/io/sancta/sanctorum/redis/CityCountry.java
@@ -1,4 +1,41 @@
 package io.sancta.sanctorum.redis;
 
+import io.sancta.sanctorum.domain.Continent;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CityCountry {
+
+    Integer id;
+
+    String name;
+
+    String district;
+
+    Integer population;
+
+    String countryCode;
+
+    String alternativeCountryCode;
+
+    String countryName;
+
+    Continent continent;
+
+    String countryRegion;
+
+    BigDecimal countrySurfaceArea;
+
+    Integer countryPopulation;
+
+    Set<Language> languages;
+
 }
--- src/main/java/io/sancta/sanctorum/redis/Language.java
+++ src/main/java/io/sancta/sanctorum/redis/Language.java
@@ -1,4 +1,21 @@
 package io.sancta.sanctorum.redis;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Language {
+
+    String language;
+
+    Boolean isOfficial;
+
+    BigDecimal percentage;
+
 }
